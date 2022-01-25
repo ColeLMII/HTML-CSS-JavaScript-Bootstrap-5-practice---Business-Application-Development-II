90c42d3d3d9ef32bc4a504ea5d1e1d2d7a43b948
ColeLMII <97560688+ColeLMII@users.noreply.github.com>
2022-01-25 16:16:32 -0600

    01/25
    oops saved to early

--- HippoLogin/js/app.js
+++ HippoLogin/js/app.js
@@ -7,10 +7,22 @@
 $(document).on('click','#btnSignIn',function(){
     console.log('btnSignIn Clicked'); // console.log is a debug feature
     let strUsername= ' '; //let local scoped variable
+     strUsername=$('#txtEmail').val(); //setting value of strUSername
 
-    strUsername=$('#txtEmail').val(); //setting value of strUSername
     console.log('Set value of strUsername = ' + strUsername); //concatenation?
     localStorage.setItem('Username', strUsername);
     console.log('Set localStorage Username');
+
+    //showing the password in clear text
+    console.log($('#txtPassword').val());
+
+    //error handing with truthy stats
+
+    if($('#txtPassword').val()){
+        console.log('Password Exist');
     }
-);
+    else{
+        console.log('Password Blank');
+    }
+
+})
