cd2876713c5d9180d9d6ff58abb744f9679a4d8b
ColeLMII <97560688+ColeLMII@users.noreply.github.com>
2022-01-27 16:19:49 -0600

    01/27 ummm alot of thing went down.

--- HippoLogin/js/app.js
+++ HippoLogin/js/app.js
@@ -4,25 +4,50 @@
 
 //alert("from app.js"); //simple javascript alert
 
-$(document).on('click','#btnSignIn',function(){
-    console.log('btnSignIn Clicked'); // console.log is a debug feature
-    let strUsername= ' '; //let local scoped variable
-     strUsername=$('#txtEmail').val(); //setting value of strUSername
+$(document).on('click','#btnSignIn',function(){ 
+    var objNewSessionResponse;
+    let blnError = false;
+    let strErrorMessage=''; // equals blank so we can use the += operator to add to it
 
-    console.log('Set value of strUsername = ' + strUsername); //concatenation?
-    localStorage.setItem('Username', strUsername);
-    console.log('Set localStorage Username');
-
-    //showing the password in clear text
-    console.log($('#txtPassword').val());
+    if(!$('#txtEmail').val()){
+        blnError=true;
+        strErrorMessage+= '<p>Email/Username is Blank.</p>'
+    }
+    if(!$('#txtPassword').val()){
+        blnError=true;
+        strErrorMessage+= '<p>Password is Blank.</p>'
+    }
 
     //error handing with truthy stats
-
-    if($('#txtPassword').val()){
-        console.log('Password Exist');
+    if(blnError == 1){
+        Swal.fire({
+            icon: 'error',
+            title: 'Missing Data',
+            html: strErrorMessage
+          })
+    }else{
+        /*
+            do not do this in production this is unprotected API
+        */
+        var objNewSessionPromise= $.post('https:www.swollenhippo.com/DS3870/newSession.php', { strUsername:$('#txtEmail').val(), strPassword:$('#txtPassword').val() }, function(result){
+            console.log(JSON.parse(result).Outcome);
+        }) 
     }
-    else{
-        console.log('Password Blank');
-    }
 
+    $.when(objNewSessionPromise).don(function(){
+        if(objNewSessionPromise.Outcome == 'Login Failed'){
+            Swal.fire({
+                icon:'error',
+                title:'Login Failed',
+                html: '<p> Review Username and Password </p>'
+            })
+        } else{
+            sessionStorage.setItem('HippoSessionID', objNewSessionPromise.Outcome);
+            Swal.fire({
+                icon: 'Success',
+                title: 'Login Complete',
+                html: '<h3> Great Job </h3>'
+            })
+        }
+    })
 })
