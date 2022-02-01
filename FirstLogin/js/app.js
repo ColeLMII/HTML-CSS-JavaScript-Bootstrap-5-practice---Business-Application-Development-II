b0c9a1f1ffe28fe86208cad6df1c9e4b4b8da694
ColeLMII <97560688+ColeLMII@users.noreply.github.com>
2022-01-31 22:11:50 -0600

    FirstLoginHw1/31

---
+++ FirstLogin/js/app.js
@@ -0,0 +1,68 @@
+$(document).on('click','#btnSignIn',function(){ 
+    var objNewSessionResponse;
+    let blnError = false;
+    let strErrorMessage=''; // equals blank so we can use the += operator to add to it
+
+    if(!$('#txtEmail').val()){
+        blnError=true;
+        strErrorMessage+= '<p>Email/Username is Blank.</p>'
+    }else if(!validateUsername($('#txtEmail').val())){
+        console.log("Bad Username");
+        blnError=true;
+        strErrorMessage+='<p>Email/Username is not valid</p>'
+    }
+
+
+    if(!$('#txtPassword').val()){
+        blnError=true;
+        strErrorMessage+= '<p>Password is Blank.</p>'
+    }else if(!validatePassword($('#txtPassword').val())){
+        console.log('Bad Password');
+        blnError=true;
+        strErrorMessage+='<p>Password is not valid</p>'
+    }
+
+    //error handing with truthy stats
+    if(blnError == 1){
+        Swal.fire({
+            icon: 'error',
+            title: 'Missing Data',
+            html: strErrorMessage
+          })
+    }else{
+        /*
+            do not do this in production this is unprotected API
+        */
+        var objNewSessionPromise= $.post('https:www.swollenhippo.com/DS3870/newSession.php', { strUsername:$('#txtEmail').val(), strPassword:$('#txtPassword').val() }, function(result){
+            console.log(JSON.parse(result).Outcome);
+        }) 
+        $.when(objNewSessionPromise).done(function(){
+            if(objNewSessionPromise.Outcome == 'Login Failed'){
+                Swal.fire({
+                    icon:'error',
+                    title:'Login Failed',
+                    html: '<p> Review Username and Password </p>'
+                })
+            } else{
+                sessionStorage.setItem('HippoSessionID', objNewSessionPromise.Outcome);
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Login Complete',
+                    html: '<h3> Great Job </h3>'
+                })
+            }
+        })
+    }
+
+    
+})
+
+function validateUsername(strUsername){
+    let reg = /\S+@\S+\.\S+/;
+    return reg.test(strUsername);
+}
+
+function validatePassword(strPassword){
+    let reg= /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+    return reg.test(strPassword);
+}
