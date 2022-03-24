2d4eb06398e738ca31843bd5ab9d6746414077f1
ColeLMII <97560688+ColeLMII@users.noreply.github.com>
2022-03-24 15:25:50 -0500

    inprogress

---
+++ Visual Studio 2019 Files/Working with MSSQL/DataIntegrationSQL/DataIntegrationSQL/Function1.cs
@@ -0,0 +1,34 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Data.SqlClient;
+using System.Data; //in order to use the dataset functions
+
+namespace DataIntegrationSQL
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function utilizes Data Integration");
+
+            string name = req.Query["name"];
+
+            string conTasks = ;
+            string strQuery = ;
+
+            
+
+            return new OkObjectResult("Test");
+        }
+    }
+}
